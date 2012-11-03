d0b2bf5604a820614dd00e0dba6b32bbdbf98943
LouisNDupuis <louisnicolasdupuis@gmail.com>
2012-11-03 14:39:52 -0400

    exo1.1

---
+++ WebSite1/Default.aspx.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+/**
+ * class
+ */
+public partial class _Default : System.Web.UI.Page
+{
+    /* page load */
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        
+    }
+    // from the exemple
+    protected void TimeLabel_Load(object sender, EventArgs e)
+    {
+        TimeLabel.Text = DateTime.Now.ToString();
+    }
+}
